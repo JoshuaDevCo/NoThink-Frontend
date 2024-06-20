775ba15baeb5a9997d2e63fa0f56d32fccbdd1b5
nevermindhim <scott.brown0629@gmail.com>
2024-06-20 18:53:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { Buffer } from "buffer";
-import nodePolyfills from "rollup-plugin-polyfill-node";
-import inject from "@rollup/plugin-inject";
+// import { Buffer } from "buffer";
+// import nodePolyfills from "rollup-plugin-polyfill-node";
+// import inject from "@rollup/plugin-inject";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
     },
+  },
+  build: {
+    // rollupOptions: {
+    //   // plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+    // },
   },
 });
